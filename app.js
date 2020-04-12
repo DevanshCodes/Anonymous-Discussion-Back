45712fc231fa504dddf8e96dbe4aaba8fc8e60b3
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-04-12 15:16:07 +0530

    fixed the sockets

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const chats = require('./models/chats')
 const app = require('express')();
-var port_number = process.env.PORT || 3000;
+var port_number = process.env.PORT || 4000;
 const server = app.listen(port_number);
-const io = require('socket.io')(4000);
+const io = require('socket.io')(server);
 const mongoose = require('mongoose');
 const cors = require('cors');
 const mongoURL = 'mongodb://devansh:KGdhpIYEsoHSz0gr@anonymous-project-shard-00-00-e0dq9.mongodb.net:27017,anonymous-project-shard-00-01-e0dq9.mongodb.net:27017,anonymous-project-shard-00-02-e0dq9.mongodb.net:27017/test?ssl=true&replicaSet=Anonymous-Project-shard-0&authSource=admin&retryWrites=true&w=majority';
@@ -34,8 +34,24 @@ io.sockets.on('connection', function (socket, client) {
     socket.on('connect', () => {
         console.log('New Client is Connected!')
     })
-    socket.on('room',function(room){
+    socket.on('room', function (room) {
         socket.join(room);
+        console.log(`${room} joined`)
+        async function chatsfun(room) {
+            const nchats = await chats.find({ roomno: room })
+            if (nchats) {
+                return nchats
+            } else {
+                return [{ username: 'Admin', chat: `welcome to room number ${req.params.roomno}` }]
+            }
+        }
+        async function emit(room) {
+            const nchats = await chatsfun(room);
+            socket.emit('chats', nchats)
+        }
+
+        emit(room)
+
     })
     socket.on("newMessage", async (data) => {
         try {
@@ -45,7 +61,6 @@ io.sockets.on('connection', function (socket, client) {
                 roomno: data.roomno
             })
             await chat.save();
-            console.log(data);
             io.sockets.in(data.roomno).emit('newChat', data)
         } catch{
             console.log('Error')
@@ -58,14 +73,9 @@ app.get('/', function (req, res) {
     res.redirect('https://www.github.com/DevanshCodes')
 })
 
-app.get('/api/chats/:roomno', async function (req, res) {
+app.get('/api/port', async function (req, res) {
     try {
-        const nchats = await chats.find({ roomno: req.params.roomno })
-        if (nchats) {
-            return res.json(nchats)
-          } else {
-            return res.json([{username: 'Admin', chat: `welcome to room number ${req.params.roomno}`}])
-          }
+        return res.json(port_number)
     } catch (error) {
         console.log(error);
     }
